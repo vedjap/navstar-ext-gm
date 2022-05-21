49c29dabc36227937363f95e9873be8d0b6529d9
Jose Palomares <jose.palomares@uabc.edu.mx>
2022-05-20 17:45:34 -0700

    restarting development

---
+++ index.js
@@ -0,0 +1,50 @@
+import "dotenv/config";
+import schedule from "node-schedule";
+import {Auth, GetEventCount, GetTags} from "./navstar.js";
+import * as fs from "fs";
+
+
+//Load start art
+console.log(`Programm starting
+						       ...
+	   /// //////      ((((((((/ ((((.     //// ........  .......  .........    ... .....
+	  /////   //// (((((    (((( ((( ... (// ....    .   ...    ....     ...  .....   .
+	 ////     /// ////      (((  (((( .((((  ......     ...    ...      ....  ...
+	///      /// ///      ((((   ((( (((        .....  ...    ...      ...  ...
+       ////     ///, *////  (((((     (((((    ...  ..... ...     ..... ......  ...
+      ///      ///     ////( (((     #((      .......    ...       .........  ....        `);
+
+const task = schedule.scheduleJob('*/15 * * * * *',  async function (fireDate){
+	//We need to check for errors at funciton level since callback is asyncronous
+	try {
+		console.log(`Expected time: ${fireDate}  Current time: ${new Date()}`);
+		const auth  = await Auth();
+		if(auth && auth.success===true){
+			// Fetch tag id configured in .env as TAG
+			const tagId = await GetTags(auth.hash);
+			console.log(tagId);
+			// Fetch trackers and vehicles attached with said tag object.
+			// Search for unread notifications since last check
+			//			const count = await (auth.hash, trackers);
+			//if(count && count.length > 0) {
+			//	console.log(count);
+			//
+			//			}else{
+			//				console.log("There's no events to sync...");
+			//}
+		}
+	} catch (err){
+
+		console.error(err);
+	}
+});
+//The following should be executed every minute
+//const updateTagTask = schedule.scheduleJob('*/1 * * * *', async function (tagId, fireDate) {
+//	try{
+//		console.log("Another task:" + fireDate);
+//		console.log(tagId);
+//		tagId = tagId + 1 ;
+//	}catch(err){
+//		console.error(err);
+//	}
+//});
---
+++ navstar.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import {format} from "date-fns";
+
+const baseURL = "http://navstar.com.mx/api-v2/"
+let lastCheck;
+
+async function Auth(){
+	const login = process.env.USERNAME;
+	const password = process.env.PASSWORD;
+
+	const authRequest = await axios.post(baseURL+"user/auth",{
+		login, password
+	});
+	const authResponse = authRequest.data;
+	//console.log(authRequest.data);
+	//console.log(authRequest.status);
+	return authResponse;
+}
+
+async function GetTags(hash){
+	try{
+		const tag = process.env.TAG;
+
+		const tagRequest = await axios.post(baseURL+"tag/list",{
+			hash,
+			filter: tag
+		});
+		const tagResponse = tagRequest.data.list;
+		if(tagResponse && tagResponse.success === true){
+			//console.log(tagResponse);
+			//console.log();
+			return tagResponse.find(x=> x.name === tag);;
+		}
+	}catch (err){
+		console.log(err);
+	}
+}
+
+async function GetEventCount(hash){
+	try{
+		if(lastCheck === undefined){
+			lastCheck = new Date();
+		}
+		console.log("Last check: " + lastCheck);
+		const countRequest = await axios.post(baseURL + 'history/unread/count',
+			{
+				hash,
+				from:format(lastCheck, "yyyy-MM-dd hh:mm:ss"),
+				to: format(new Date(), "yyyy-MM-dd hh:mm:ss")
+			});
+		lastCheck = new Date();
+		console.log(countRequest.data);
+		if(countRequest.data.success === true){
+			return countRequest.data.count;
+		}else{
+
+			console.log("Something went wrong at trying to fetch unread events");
+		}
+	}catch(err){
+		console.log(err.response.data);
+		console.log(err);
+	}
+}
+
+
+async function GetData(tracker_id){
+
+}
+
+export {
+	Auth,
+	GetEventCount,
+	GetTags
+};
